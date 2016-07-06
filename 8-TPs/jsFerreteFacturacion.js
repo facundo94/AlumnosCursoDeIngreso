19dbf066d701e51b58068bf264e7e24790605332
Facundo94 <facu.31@live.com>
2016-07-05 21:56:41 -0300

    tp
    Signed-off-by: Facundo94 <facu.31@live.com>

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,48 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var resultado;
+
+	precioUno= parseInt(document.getElementById("PrecioUno").value);
+	precioDos= parseInt(document.getElementById("PrecioDos").value);
+	precioTres= parseInt(document.getElementById("PrecioTres").value);
+	resultado= precioUno+precioDos+precioTres;
+
+	alert(resultado);
+
 }
 function Promedio () 
 {
 	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var resultado;
+
+	precioUno= parseInt(document.getElementById("PrecioUno").value);
+	precioDos= parseInt(document.getElementById("PrecioDos").value);
+	precioTres= parseInt(document.getElementById("PrecioTres").value);
+	resultado= (precioUno+precioDos+precioTres) / 3;
+
+	alert(resultado);
+
 }
 function PrecioFinal () 
 {
 	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var resultado;
+
+	precioUno= parseInt(document.getElementById("PrecioUno").value);
+	precioDos= parseInt(document.getElementById("PrecioDos").value);
+	precioTres= parseInt(document.getElementById("PrecioTres").value);
+	resultado= (precioUno+precioDos+precioTres)*1.21;
+
+	alert(resultado);
+
 }
