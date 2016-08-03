656f4360b3cd0f67756660b9d74ec01b0762dcda
Facundo94 <facu.31@live.com>
2016-08-02 21:48:44 -0300

    Signed-off-by: Facundo94 <facu.31@live.com>

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -7,7 +7,90 @@ c.	Se pedirán un número positivo y se mostrará la cantidad de números divisi
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
+
+	
+var numero;
+var contador=0;
+var cantPares=0;
+var cantImp=0;
+var cantDiv=0;
+var primo=0;
+var primos=0;
+
 function ComenzarIngreso () 
 {
- 	
+	NumerosPares()
+	NumerosImpares()
+	NumerosDivisibles()
+
+}
+
+function NumerosPares()
+{
+	numero=parseInt(document.getElementById("numero").value);
+
+	while(numero>1){
+ 		
+ 		if (numero%2==0) {
+ 			cantPares++;
+ 		}
+
+ 		numero--;
+ 	}
+
+ 	alert("Numero pares "+cantPares);
+}
+
+function NumerosImpares() 
+{
+	numero=parseInt(document.getElementById("numero").value);
+
+ 	while(numero>=1){
+
+ 		if(numero%2==1){
+ 			cantImp++;
+ 		}
+
+ 		numero--;
+ 	}
+
+ 	alert("Numero Impares "+cantImp);
+}
+
+function NumerosDivisibles()
+{
+ 	numero=parseInt(document.getElementById("numero").value);
+ 	contador=0;
+
+	while(contador<100){
+
+ 		if (numero%contador==0) {
+ 			cantDiv++;
+ 		}
+ 		contador++;
+ 	}
+
+ 	alert("Numeros divisibles "+cantDiv);
+}
+
+function VerificarPrimo()
+{
+	numero=parseInt(document.getElementById("numero").value);
+	conador=0;
+	primo=0;
+
+	while(contador<numero){
+		contador++;
+
+		if (numero%contador==0){
+			primo++;
+		}
+	}
+
+	if(primo>2){
+		alert("No es primo");
+	}
+	else{
+		alert("Es primo");
+	}
 }
--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -7,7 +7,74 @@ D.	Sueldo bruto, no menor a 8000.
 E.	Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.
 F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.
  */
-function ComenzarIngreso () 
+function ComenzarIngreso()
 {
- 
+ 	var edad=0;
+ 	var sexo;
+ 	var estadoCivil=0;
+ 	var sueldo=0;
+ 	var legajo=0;
+ 	var nacionalidad;
+
+ 	while(edad<18 || edad>90){
+ 		edad=parseInt(prompt("Ingrese su edad"));
+ 	}
+
+ 	while(sexo!= "M" && sexo!="F"){
+ 		sexo=prompt("Indique sexo");
+ 	}
+ 	while(estadoCivil<1 || estadoCivil>4){
+ 		estadoCivil=parseInt(prompt("Estado civil"));
+ 	}
+ 	while(sueldo<8000){
+ 		sueldo=parseInt(prompt("Ingrese su sueldo"));
+ 	}
+ 	while(legajo<1000 || legajo>9999){
+ 		legajo=parseInt(prompt("Ingrese el legajo"));
+ 	}
+ 	while(nacionalidad != "A" && nacionalidad!="E" && nacionalidad!="N"){
+ 		nacionalidad=prompt("Indique su nacionalidad");
+ 	}
+
+ 	switch(sexo){
+ 		case "M":
+ 			sexo="masculino";
+ 			break;
+ 		case "F":
+ 			sexo="femenino";
+ 			break;
+ 	}
+
+ 	switch(estadoCivil){
+ 		case 1:
+ 			estadoCivil="solerto";
+ 			break;
+ 		case 2:
+ 			estadoCivil="casado";
+ 			break;
+ 		case 3:
+ 			estadoCivil="divorciado";
+ 			break;
+ 		case 4:
+ 			estadoCivil="viudo";
+ 			break;
+ 	}
+ 	switch(nacionalidad){
+ 		case "A":
+ 			nacionalidad="argentino";
+ 			break;
+ 		case "E":
+ 			nacionalidad="extranjero";
+ 			break;
+ 		case "N":
+ 			nacionalidad="nacionalizado";
+ 			break;
+ 	}
+ 	document.getElementById("Edad").value=edad;
+ 	document.getElementById("Sexo").value=sexo;
+ 	document.getElementById("EstadoCivil").value=estadoCivil;
+ 	document.getElementById("Sueldo").value=sueldo;
+ 	document.getElementById("Legajo").value=legajo;
+ 	document.getElementById("Nacionalidad").value=nacionalidad;
+
 }
